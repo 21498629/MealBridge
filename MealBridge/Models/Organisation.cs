d693319d45cba071271ae4e0cef1cf71fd46eac3
21498629 <u21498629@tuks.co.za>
2024-07-11 20:39:58 +0200

    Add project files.

---
+++ MealBridge/Models/Organisation.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MealBridge.Models
+{
+    public class Organisation
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string ContactNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+
+        public List<OrganisationType> OrganisationTypes { get; set; } = new List<OrganisationType>();
+    }
+}
---
+++ MealBridge/Models/Repository.cs
@@ -0,0 +1,59 @@
+﻿using MealBridge.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MealBridge.Models
+{
+    public class Repository : IRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public Repository(AppDbContext appDbContext) 
+        {
+            _appDbContext = appDbContext;
+        }
+
+        // Saving changes
+        public async Task<bool>SaveChangesAsync()
+        {
+            return await _appDbContext.SaveChangesAsync() > 0;
+        }
+
+        // Adding
+        public void Add<T>(T entity) where T : class 
+        {
+            _appDbContext.Add(entity);
+        }
+
+        // Delete
+        public void Delete<T>(T entity) where T : class 
+        {
+            _appDbContext.Remove(entity);
+        }
+
+        // Organisation
+        public async Task<Organisation[]> GetAllOrganisationsAsync()
+        {
+            IQueryable<Organisation> query = _appDbContext.Organisations;
+            return await query.ToArrayAsync();
+        }
+
+        public async Task<Organisation> GetOrganisationByIdAsync(int OrganisationId)
+        {
+            IQueryable<Organisation> query = _appDbContext.Organisations;
+            return await query.FirstOrDefaultAsync();
+        }
+
+        // Organisation Type
+        public async Task<OrganisationType[]> GetAllOrganisationTypesAsync()
+        {
+            IQueryable<OrganisationType> query = _appDbContext.OrganisationTypes;
+            return await query.ToArrayAsync();
+        }
+
+        public async Task<OrganisationType> GetOrganisationTypeByIdAsync(int OrganisationTypeId)
+        {
+            IQueryable<OrganisationType> query = _appDbContext.OrganisationTypes;
+            return await query.FirstOrDefaultAsync();
+        }
+    }
+}
